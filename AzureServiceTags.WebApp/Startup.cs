91d4c26120b20d5ca5891087ff7bdcfb5185c86d
Jelle Druyts <jelle_druyts@yahoo.com>
2021-07-19 12:27:50 +0200

    Enabled AppInsights

--- AzureServiceTags.WebApp/Startup.cs
+++ AzureServiceTags.WebApp/Startup.cs
@@ -19,6 +19,7 @@ namespace AzureServiceTags.WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry();
             services.AddHttpClient();
             services.AddSingleton<ServiceTagProvider>();
             services.AddRazorPages().AddRazorRuntimeCompilation();
